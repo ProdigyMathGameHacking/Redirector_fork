fec14361cb892921c5fb5c892f6adbe29c9bbe45
gemsvido <gemsvido@gmail.com>
2022-07-23 16:10:34 -0700

    Hell yeah dawg

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ const fetch = require("node-fetch");
 
 (async () => {
     const status = await (await fetch("https://api.prodigygame.com/game-api/status")).json();
-    const gameMin = await (await fetch(`${status.data.gameCodePath}game.min.js`)).text();
+    const gameMin = new String(await (await fetch(`${status.data.gameCodePath}game.min.js`)).text()).valueOf();
     console.log(gameMin.length);
 })();
